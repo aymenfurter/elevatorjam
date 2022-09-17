aaa7cd1c960ef99495b0c1edf1109649b189c38f
Abbas Jafari <abbas.jafari@powercoders.org>
2022-09-17 10:32:05 +0200

    fix: cors

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,19 @@ from fastapi.responses import HTMLResponse
 from pathlib import Path
 from backend.controllers.main import route
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = ["*"]
 app = FastAPI(version="1.0.0", title="server")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router=route)
 
 
