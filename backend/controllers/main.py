183a35e03a0fe8df51f8d27d60426e8500bf2370
Abbas Jafari <abbas.jafari@powercoders.org>
2022-09-17 09:12:58 +0200

    push

--- backend/controllers/main.py
+++ backend/controllers/main.py
@@ -4,11 +4,15 @@ from backend.models.models import STATE
 
 route = APIRouter()
 
-@route.post("/user/{id}/{song}")
-async def add_user(id: str, song: str) -> str:
-    if id in list(user.uid for user in STATE.users):
-        next(map(lambda x: x.songs.append(song) , [user for user in STATE.users if user.uid == id]))
+@route.post("/user")
+async def add_user(userId: str, music: str) -> str:
+    if userId in list(user.uid for user in STATE.users):
+        next(map(lambda x: x.songs.append(music) , [user for user in STATE.users if user.uid == userId]))
     else:
-        STATE.users.append(User(uid=id, songs =[song]))
+        STATE.users.append(User(uid=userId, songs =[music]))
     print(STATE.users)
     return "ok"
+
+@route.post("/enter")
+async def enters(userId: str, elevatorId: str):
+    pass
